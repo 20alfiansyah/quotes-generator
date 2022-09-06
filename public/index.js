c2056be807792c1c64cb37c305ce7d575da4bb4d
20alfiansyah <alfiansyahhidayat200@gmail.com>
2022-09-06 23:15:35 +0700

    first commit

---
+++ public/index.js
@@ -0,0 +1,26 @@
+const inputNama = document.getElementById('inputNama')
+const result = document.getElementById('result')
+const waitText = document.getElementById("waitText")
+const imgHeart = document.getElementById("imgHeart")
+const quotes = ["Stop stressing over shitty people", "Be like a sun keep on shining and let them burn","let it hurt then let it go"]
+function clickMe() {
+    let random = Math.floor(Math.random() * quotes.length)
+    wrongInput.classList.add('hidden')
+    waitText.classList.toggle('hidden')
+    setTimeout(() => {
+        if (inputNama.value.length == 0) {
+            waitText.classList.toggle('hidden')
+            wrongInput.classList.remove('hidden')
+            result.classList.add('hidden')
+        }
+        else{
+            waitText.classList.toggle('hidden')
+            result.classList.remove('hidden')
+            result.innerHTML = "<span class ='text-red-700'>"+ inputNama.value + "</span>" + " Quotes yang kamu dapat " + "<span class ='text-red-700'>"+ '"' + quotes[random] + '"' + "</span>"
+        }
+    }, 2000);
+}
+
+function heart(){
+    imgHeart.classList.toggle("grayscale" )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'poppins':'Poppins',
+        'pacifico' : 'Pacifico'
+      },
+      height:{
+        '2000':'755px'
+      }
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+  },
+  plugins: [],
+}
